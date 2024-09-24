00fbc41b151d119073df5a9a16d3aa9e751a029a
ramyzhang <ramyjzh@gmail.com>
2024-09-24 01:43:15 -0400

    moved math to source file

--- minimi-engine/Math/Vec2.hpp
+++ minimi-engine/Math/Vec2.hpp
@@ -19,46 +19,21 @@ public:
     Vec2() {};
     Vec2(float _x, float _y) : x(_x), y(_y) {};
     
-    Vec2 operator + (const Vec2& rhs) const {
-        return Vec2(x + rhs.x, y + rhs.y);
-    }
+    Vec2 operator + (const Vec2& rhs) const;
     
-    Vec2 operator - (const Vec2& rhs) const {
-        return Vec2(x - rhs.x, y - rhs.y);
-    }
+    Vec2 operator - (const Vec2& rhs) const;
     
-    Vec2 operator * (const float& rhs) const {
-        return Vec2(x * rhs, y * rhs);
-    }
+    Vec2 operator * (const float& rhs) const;
     
-    Vec2& add(const Vec2& rhs) {
-        x += rhs.x;
-        y += rhs.y;
-        return *this;
-    }
+    Vec2& add(const Vec2& rhs);
     
-    Vec2& subtract(const Vec2& rhs) {
-        x -= rhs.x;
-        y -= rhs.y;
-        return *this;
-    }
+    Vec2& subtract(const Vec2& rhs);
     
-    Vec2& scale(const float& rhs) {
-        x *= rhs;
-        y *= rhs;
-        return *this;
-    }
+    Vec2& scale(const float& rhs);
     
-    Vec2& rotate(const float& deg) {
-        float rads = deg * M_PI / 180.0;
-        x = cosf(rads) * x - sinf(rads) * y;
-        y = sinf(rads) * x + cosf(rads) * y;
-        return *this;
-    }
+    Vec2& rotate(const float& deg);
     
-    float distance(const Vec2& v) const {
-        return sqrtf((v.x - x) * (v.x - x) + (v.y - y) * (v.y - y));
-    }
+    float distance(const Vec2& v) const;
 };
 
 #endif /* Vec2_hpp */
